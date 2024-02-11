3708c67cae37d46f7eb87c7b7b058cb99aeb9db9
Shubham Bodane <shubham.bodane@siemens.com>
2024-02-11 23:33:38 +0530

    added code for NgIf and NgFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { RoomsComponent } from "./rooms/rooms.component";
+import { RoomsComponent } from './rooms/rooms.component';
 @Component({
-    selector: 'app-root',
-    standalone: true,
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.scss',
-    imports: [RouterOutlet, RoomsComponent]
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+  imports: [RouterOutlet, RoomsComponent],
 })
 export class AppComponent {
   title = 'hotelinventoryapp';
--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -1,17 +1,46 @@
 import { Component } from '@angular/core';
+import { Room, RoomList } from './rooms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-rooms',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './rooms.component.html',
-  styleUrl: './rooms.component.scss'
+  styleUrl: './rooms.component.scss',
 })
 export class RoomsComponent {
   hotelName = 'the conrad';
   numberOfRooms = 8;
 
   hideRooms = false;
+
+  rooms: Room = {
+    totalRooms: 20,
+    availableRooms: 10,
+    bookedRooms: 5,
+  };
+
+  roomList: RoomList[] = [
+    {
+      roomNumber: 1,
+      roomType: 'Deluxe Room',
+      amenities: 'television, wifi, tv, AC',
+      checkinTime: new Date('11-Nov-2023'),
+      checkoutTime: new Date('12-Nov-2023'),
+      price: 1234,
+      photos: 'test',
+    },
+    {
+      roomNumber: 2,
+      roomType: 'Normal Room',
+      amenities: 'television, wifi, ',
+      checkinTime: new Date('20-Nov-2023'),
+      checkoutTime: new Date('27-Nov-2023'),
+      price: 1000,
+      photos: 'test1',
+    },
+  ];
 
   toggle() {
     this.hideRooms = !this.hideRooms;
