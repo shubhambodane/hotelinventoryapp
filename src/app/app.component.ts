643e3afc6bfc3e3a55ecb1f1af5d681cb405ac4d
Shubham Bodane <shubham.bodane@siemens.com>
2024-02-11 12:47:10 +0530

    added new rooms component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { RoomsComponent } from "./rooms/rooms.component";
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [RouterOutlet, RoomsComponent]
 })
 export class AppComponent {
   title = 'hotelinventoryapp';
